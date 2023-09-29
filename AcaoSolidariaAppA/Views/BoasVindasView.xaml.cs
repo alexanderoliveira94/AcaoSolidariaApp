5c186b0ab5ce629cf92be0e267bb3084bcb3efe3
Alexander <alexander.oliveira3@etec.sp.gov.br>
2023-09-29 00:32:58 -0300

    Segunda versao, tela de login e botões

--- AcaoSolidariaAppA/Views/BoasVindasView.xaml.cs
+++ AcaoSolidariaAppA/Views/BoasVindasView.xaml.cs
@@ -1,14 +1,32 @@
+using System;
+using Microsoft.Maui.Controls;
+
 namespace AcaoSolidariaAppA.Views;
 
 public partial class BoasVindasView : ContentPage
 {
 	public BoasVindasView()
 	{
-		InitializeComponent();
+        InitializeComponent();
 	}
 
-    private async void ComecarBtn(object sender, EventArgs e)
+    private void ComecarBtn_Cliked(object sender, EventArgs e)
     {
-        await Navigation.PushAsync(new Page());
+        // Adicione a lógica para lidar com o botão "Entrar" aqui
+    }
+
+    private void EntrarBtn_Clicked(object sender, EventArgs e)
+    {
+        // Adicione a lógica para lidar com o botão "Entrar" aqui
+    }
+
+    private void CadastrarBtn_Clicked(object sender, EventArgs e)
+    {
+        // Adicione a lógica para lidar com o botão "Cadastrar-se" aqui
+    }
+
+    private void EsqueceuSenhaBtn_Clicked(object sender, EventArgs e)
+    {
+        // Adicione a lógica para lidar com o botão "Esqueceu a senha" aqui
     }
 }
